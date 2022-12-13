1062274afbfa551ba2acd9a4bced538409cd38ab
Nate <natemorrison28@gmail.com>
2022-12-13 17:58:24 -0500

    Fixes env file and sets up test route for db

--- api/express.js
+++ api/express.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 
 const {Pool} = require('pg');
 
@@ -17,12 +18,18 @@ const PORT = 8000;
 
 //Sets up the pool for the server
 const pool = new Pool({
-    connectionString: process.env.PGCONNECT
+    connectionString: process.env.PG_CONNECT
 });
 pool.connect();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/api/users', (req, res) => {
+    pool.query('SELECT * FROM cohorts')
+    .then(result => res.send(result.rows))
+    .catch(error => res.send(error))
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
