eb411b1e0d180a834aa7a9f8be267607fbd24382
KatieChevy <K.chevez4673@gmail.com>
2022-12-14 13:44:32 -0700

    adds student list

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Home } from './routes/Home';
+
 
 export const App = () => {
   return (
--- app/src/components/StudentList.jsx
+++ app/src/components/StudentList.jsx
@@ -1,10 +1,54 @@
 import {StudentStats} from './StudentStats';
 import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
 
 export const StudentList = () => {
   return(
     <div id="student-list">
-      <p>Student list</p>
+      <Table id="student-list-table" striped bordered hover>
+      <thead>
+        <tr>
+          <th>Student</th>
+          <th>Performance</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Dylan Clark</td>
+          <td>
+            <ButtonGroup size="lg" className="lg">
+              <Button className="red">10</Button>
+              <Button className="yellow">50</Button>
+              <Button className="green">100</Button>
+            </ButtonGroup>
+          </td>
+        </tr>
+        <tr>
+          <td>Dylan Clark</td>
+          <td>
+            <ButtonGroup size="lg" className="lg">
+              <Button className="red">10</Button>
+              <Button className="yellow">50</Button>
+              <Button className="green">100</Button>
+            </ButtonGroup>
+          </td>
+        </tr>
+        <tr>
+          <td>Dylan Clark</td>
+          <td>
+            <ButtonGroup size="lg" className="lg">
+              <Button className="red">10</Button>
+              <Button className="yellow">50</Button>
+              <Button className="green">100</Button>
+            </ButtonGroup>
+          </td>
+        </tr>
+       
+      </tbody>
+    </Table>
     </div>
   );
 }
