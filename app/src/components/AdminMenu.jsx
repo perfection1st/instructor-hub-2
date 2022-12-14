249e449176aa807c8aca9069bf5592fc23c711c0
perfection1st <0v3rkillgaming@gmail.com>
2022-12-14 17:32:39 -0500

    Fixes conflicts

--- app/src/components/AdminMenu.jsx
+++ app/src/components/AdminMenu.jsx
@@ -5,16 +5,7 @@ import { GoGear, GoPerson, GoSignOut } from "react-icons/go";
 export const AdminMenu = () => {
   return(
     <div id="admin-menu">
-    <DropdownButton
-      align="end"
-      title="Admin"
-      menuVariant="dark"
-      id="dropdown-menu-align-end"
-    >
-      <Dropdown.Item eventKey="1"><GoGear /> Settings</Dropdown.Item>
-      <Dropdown.Divider />
-      <Dropdown.Item eventKey="4"><GoSignOut /> Logout</Dropdown.Item>
-    </DropdownButton>
+      <p>Admin menu goes here</p>
     </div>
   );
 }
