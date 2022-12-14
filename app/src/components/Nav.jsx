13c525ab5839649eddaf0cb207aec98b6d66a9a6
perfection1st <0v3rkillgaming@gmail.com>
2022-12-14 17:31:03 -0500

    Fixes conflicts

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -3,9 +3,9 @@ import { GoPlus } from "react-icons/go";
 export const Nav = () => {
   return (
     <nav>
-      <button id="btn-weekly-update">Weekly Update <GoPlus /></button>
-      <button id="btn-project-update">Project Update <GoPlus /></button>
-      <button id="btn-assessment-update">Assessments <GoPlus /></button>
+      <button id="btn-weekly-update">Weekly Update</button>
+      <button id="btn-project-update">Project Update</button>
+      <button id="btn-assessment-update">Assessments</button>
     </nav>
   );
 }
