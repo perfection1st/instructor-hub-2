96b11ecd3ede123f97ebbc3846b5de077571430c
perfection1st <0v3rkillgaming@gmail.com>
2022-12-14 17:24:35 -0500

    Adds admin panel and cohort selection

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -1,9 +1,11 @@
+import { GoPlus } from "react-icons/go";
+
 export const Nav = () => {
   return (
     <nav>
-      <button id="btn-weekly-update">Weekly Update</button>
-      <button id="btn-project-update">Project Update</button>
-      <button id="btn-assessment-update">Assessments</button>
+      <button id="btn-weekly-update">Weekly Update <GoPlus /></button>
+      <button id="btn-project-update">Project Update <GoPlus /></button>
+      <button id="btn-assessment-update">Assessments <GoPlus /></button>
     </nav>
   );
 }
