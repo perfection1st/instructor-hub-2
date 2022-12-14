74dca49a09d22a17d0255552f08e974628680bd2
timothyiott <timothyiott@yahoo.com>
2022-12-14 13:35:12 -0900

    added user form to login and register pages

--- app/src/routes/Login.jsx
+++ app/src/routes/Login.jsx
@@ -1,10 +1,20 @@
-import { Navigate, Link } from 'react-router-dom'
+import { Navigate, Link } from 'react-router-dom';
+import { useRef } from 'react';
 
 export const Login = (props) => {
 
   const { isLoggedIn, setIsLoggedIn } = props
+  const usernameRef = useRef();
+  const passwordRef = useRef();
 
   const loginHandler = () => {
+    let username = usernameRef.current.value
+    let password = passwordRef.current.value
+    /*
+      insert function to authenticate user with the username and password
+
+      the setIsLoggedIn function will have to be placed in a async function that waits on the authentication to come back successfully
+    */
     setIsLoggedIn(true)
   }
 
@@ -15,11 +25,17 @@ export const Login = (props) => {
 
   return(
     <>
-      <p>This is login component</p>
-      <button onClick={loginHandler}>Login</button>
-      <Link to='/register'>
-        <button>Create Account</button>
-      </Link>
+      <h2>SIGN IN</h2>
+      <form>
+        <label>Username</label>
+        <input ref={usernameRef} type="text" placeholder="type username here" required />
+        <label>Password</label>
+        <input ref={passwordRef} type="password" placeholder="type password here" minLength={8} required />
+        <input type="submit" value="Sign In" onClick={loginHandler} />
+      </form>
+      <p>
+        Don't have an account? Click <Link to='/register'>Here</Link>
+      </p>
     </>
   );
 }
--- app/src/routes/Register.jsx
+++ app/src/routes/Register.jsx
@@ -1,10 +1,27 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, Link } from 'react-router-dom';
+import { useRef } from 'react';
 
 export const Register = (props) => {
 
   const { isLoggedIn, setIsLoggedIn } = props
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+  const asanaApiKeyRef = useRef();
 
   const loginHandler = () => {
+    let username = usernameRef.current.value
+    let password
+    if(passwordRef.current.value !== confirmPasswordRef.current.value){
+      alert('passwords don\'t match, please try again!');
+    } else {
+      password = passwordRef.current.value;
+    }
+    /*
+      insert function to create new user with the username and password
+
+      the setIsLoggedIn function will have to be placed in a async function that waits on the account creation verification to come back successfully
+    */
     setIsLoggedIn(true)
   }
 
@@ -15,10 +32,21 @@ export const Register = (props) => {
 
   return(
     <>
-      <p>Register component</p>
-      <button onClick={loginHandler}>
-        Register
-      </button>
+      <h2>REGISTER</h2>
+      <form>
+        <label>Username</label>
+        <input ref={usernameRef} type="text" placeholder="type username here" required />
+        <label>Password</label>
+        <input ref={passwordRef} type="password" placeholder="type password here" minLength={8} required />
+        <label>Confirm Password</label>
+        <input ref={confirmPasswordRef} type="password" placeholder="confirm password here" minLength={8} required />
+        <label>Asana API Key</label>
+        <input ref={asanaApiKeyRef} type="text" placeholder="type API key here" required />
+        <input type="submit" value="Register" onClick={loginHandler} />
+      </form>
+      <p>
+        Already have an account? Click <Link to='/login'>Here</Link> to sign in.
+      </p>
     </>
   );
 }
