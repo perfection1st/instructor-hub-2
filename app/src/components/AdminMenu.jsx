d15c99a41c6a72c2ebb10a429470cc0e91df0ee2
perfection1st <0v3rkillgaming@gmail.com>
2022-12-14 17:47:55 -0500

    Admin panel/button icons

--- app/src/components/AdminMenu.jsx
+++ app/src/components/AdminMenu.jsx
@@ -1,7 +1,20 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import { GoGear, GoSignOut } from "react-icons/go";
+
 export const AdminMenu = () => {
   return(
     <div id="admin-menu">
-      <p>Admin menu goes here</p>
+    <DropdownButton
+      align="end"
+      title="Admin"
+      menuVariant="dark"
+      id="dropdown-menu-align-end"
+    >
+      <Dropdown.Item eventKey="1"><GoGear /> Settings</Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item eventKey="4"><GoSignOut /> Logout</Dropdown.Item>
+    </DropdownButton>
     </div>
   );
 }
