2d8b132b988c3900fbc8f0dfbed73496f63b88ed
perfection1st <0v3rkillgaming@gmail.com>
2022-12-14 17:34:08 -0500

    Fixes conflicts

--- app/src/components/AdminMenu.jsx
+++ app/src/components/AdminMenu.jsx
@@ -1,7 +1,3 @@
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import { GoGear, GoPerson, GoSignOut } from "react-icons/go";
-
 export const AdminMenu = () => {
   return(
     <div id="admin-menu">
