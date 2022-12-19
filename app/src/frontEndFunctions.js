824fc3a85bd4aedff487c4808adea0379c800de7
AdamDanielJones <adamdjonesbusiness@gmail.com>
2022-12-19 12:54:43 -0500

    adds dynamic GID and dynamic authorization token to get request for modals

--- app/src/frontEndFunctions.js
+++ app/src/frontEndFunctions.js
@@ -1,11 +1,13 @@
 import { useEffect, useState, useMemo } from "react"
 
+
+
 export const StudentsList = () => {
     const [studentsState, setStudentsState] = useState([])
     useEffect(() => {
-        fetch(`https://app.asana.com/api/1.0/projects/1203169000891327/tasks`, {
+        fetch(`https://app.asana.com/api/1.0/projects/${sessionStorage.getItem('GID')}/tasks`, {
             headers: {
-                Authorization: `Bearer 1/1202887090235848:e50a6b5c7c5a8d420afa41cb62b558d0`
+                Authorization: `Bearer ${sessionStorage.getItem('asanaToken')}`
             }
         })
         .then(result => result.json())
