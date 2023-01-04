0902436fd832d23fdd5a3ff385db5fa00a3045f7
Nate <natemorrison28@gmail.com>
2023-01-04 15:58:26 -0500

    Fixes create cohort

--- api/express.js
+++ api/express.js
@@ -168,7 +168,7 @@ app.post(`/api/create/cohort`, (req, res) => {
     // gets cohort object from body
     const newCohort = req.body.newCohort
     // updates cohort table inside database
-    pool.query(`INSERT INTO cohorts (cohort_name, begin_date, end_date, instructor, gid) VALUES ($1, $2, $3, $4, $5) RETURNING *`, [newCohort.name, newCohort.begin_date, newCohort.end_date, newCohort.instructor, newCohort.gid])
+    pool.query(`INSERT INTO cohorts (cohort_name, begin_date, end_date, instructor) VALUES ($1, $2, $3, $4) RETURNING *`, [newCohort.name, newCohort.begin_date, newCohort.end_date, newCohort.instructor])
         .then(result => res.status(200).send(result.rows))
         .catch(error => res.status(404).send(error))
 })
