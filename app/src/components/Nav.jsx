97b5d99ac741e30e05274aee94c9f51f36e3e771
timothyiott <timothyiott@yahoo.com>
2023-01-04 13:54:03 -0900

    fixed bugs in the assessment modal and in the weekly modal

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -10,12 +10,10 @@ export const Nav = (props) => {
   const { courses, setCourses } = props
 
   const [selectedStudents, setSelectedStudents] = useState([])
-  const [checked, setChecked] = useState(false)
-
 
   return (
     <nav>
-      <WeeklyModal checked={checked} setChecked={setChecked} courses={courses} selectedStudents={selectedStudents} setSelectedStudents={setSelectedStudents} />
+      <WeeklyModal courses={courses} selectedStudents={selectedStudents} setSelectedStudents={setSelectedStudents} />
       <ProjectModal courses={courses} selectedStudents={selectedStudents} setSelectedStudents={setSelectedStudents} />
       <AssessmentModal courses={courses} selectedStudents={selectedStudents} setSelectedStudents={setSelectedStudents} />
     </nav>
