1532edadbecc7444ce80b09c77de938ac4a37de7
roymansoor <roy.mansoor@gmail.com>
2023-01-26 19:38:40 -0600

    Change name is done!

--- app/src/components/ChangeStudentName.jsx
+++ app/src/components/ChangeStudentName.jsx
@@ -1,81 +1,3 @@
-// import React from "react";
-// import { useState, useRef, useEffect } from "react";
-// import swal from "sweetalert";
-// import { BsPencilFill } from "react-icons/bs";
-// import "../css/ChangeStudentName.css";
-
-// export const ChangeStudentName = (props) => {
-//   const URL = "http://localhost:8000";
-//   const [studentNewName, setStudentNewName] = useState();
-//   const [isClicked, setIsClicked] = useState(false);
-
-//   const { clickedStudent, setClickedStudent, loadStudents } = props;
-
-//   const newNameRef = useRef();
-
-//   function ChangeName() {
-//     let newName = newNameRef.current.value;
-//     fetch(`${URL}/api/students/nameChange`, {
-//       method: "PATCH",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
-//       },
-//       body: JSON.stringify({
-//         name: `${newName} `,
-//         cohort_name: sessionStorage.getItem("currentClass"),
-//         oldName: clickedStudent,
-//       }),
-//     }).then(() => {
-//       swal("Name Changed!");
-//       loadStudents(sessionStorage.getItem("currentClass"));
-//       setClickedStudent(newName);
-//     });
-
-//     //console.log(newNameRef.current.value);
-//     // if (newName == "") {
-//     //   swal("Please fill out all fields");
-//     // } else {
-//     //   setStudentNewName({
-//     //     name: `${newName} `,
-//     //     cohort_name: sessionStorage.getItem("currentClass"),
-//     //     oldName: clickedStudent,
-//     //   });
-
-//     // }
-//   }
-//   //console.log(studentNewName);
-//   const handleClick = () => {
-//     setIsClicked(!isClicked);
-//   };
-
-//   return (
-//     <div className="pen-change-name">
-//       <a className="pen" onClick={handleClick}>
-//         <BsPencilFill />
-//       </a>
-//       {isClicked ? (
-//         <div className="pen-input">
-//           <input
-//             className="form-control"
-//             placeholder="Enter New First & Last Name"
-//             ref={newNameRef}
-//             type="text"
-//             name="name"
-//           ></input>
-//           <button className="btn btn-primary" onClick={ChangeName}>
-//             Save
-//           </button>
-//         </div>
-//       ) : (
-//         <p className="change-text">Change Name </p>
-//       )}
-//     </div>
-//   );
-// };
-
-
-
 import React from "react";
 import { useState, useRef, useEffect } from "react";
 import swal from "sweetalert";
@@ -93,13 +15,13 @@ export const ChangeStudentName = (props) => {
 
   function ChangeName() {
     let newName = newNameRef.current.value;
-    
-//console.log(newNameRef.current.value);
-    if (newName == "") {
-      swal("Please fill out all fields");
+    let nameRegex = /^([a-zA-ZÀ-ÿ]+)(\s[a-zA-ZÀ-ÿ]+)?\s([a-zA-ZÀ-ÿ]+)$/;
+    //console.log(newNameRef.current.value);
+    if (!nameRegex.test(newName)) {
+      swal("Write your first optional middle and last name ONLY");
     } else {
       setStudentNewName({
-        name: `${newName}` ,
+        name: `${newName}`,
         cohort_name: sessionStorage.getItem("currentClass"),
         oldName: clickedStudent,
       });
@@ -111,7 +33,7 @@ export const ChangeStudentName = (props) => {
           Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
         },
         body: JSON.stringify({
-          name: `${newName}` ,
+          name: `${newName}`,
           cohort_name: sessionStorage.getItem("currentClass"),
           oldName: clickedStudent,
         }),
@@ -119,8 +41,6 @@ export const ChangeStudentName = (props) => {
         swal("Name Changed!");
         loadStudents(sessionStorage.getItem("currentClass"));
       });
-
-
     }
   }
   //console.log(studentNewName);
@@ -142,7 +62,13 @@ export const ChangeStudentName = (props) => {
             type="text"
             name="name"
           ></input>
-          <button className="btn btn-primary" onClick={ChangeName}>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              ChangeName();
+              handleClick();
+            }}
+          >
             Save
           </button>
         </div>
