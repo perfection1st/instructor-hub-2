0a627c432ac8f7ba85c22ea53d414985965909e8
Lordprettyflacko <snsolomar@gmail.com>
2023-01-30 15:16:06 -0600

    second

--- app/src/setupTests.js
+++ app/src/setupTests.js
@@ -2,9 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-import 'jest-canvas-mock';
+import "@testing-library/jest-dom";
+import "jest-canvas-mock";
 
-jest.mock('react-chartjs-2', () => ({
-  Bar: () => null
+jest.mock("react-chartjs-2", () => ({
+  Bar: () => null,
 }));
