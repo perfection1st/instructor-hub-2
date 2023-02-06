b011107580fb45fdc098a4ab5408c0ae4fcd6a65
Roohullah Mansoor <42701612+roymansoor@users.noreply.github.com>
2023-02-06 12:59:35 -0600

    Loop (#20)
    * selenium test setup
    
    * updating
    
    * tests are done and comments are added
    
    ---------
    
    Co-authored-by: VitaVelMors <bwhitemedic@icloud.com>

--- app/src/components/ChangeStudentName.jsx
+++ app/src/components/ChangeStudentName.jsx
@@ -1,31 +1,37 @@
 import React from "react";
 import { useState, useRef, useEffect } from "react";
-import swal from "sweetalert";
-import { BsPencilFill } from "react-icons/bs";
-import "../css/ChangeStudentName.css";
+import swal from "sweetalert"; // Sweetalert library for displaying alerts
+import { BsPencilFill } from "react-icons/bs"; // React-icons library for pencil icon
+import "../css/ChangeStudentName.css"; // CSS styles for the component
 
+// Function component to change a student's name
 export const ChangeStudentName = (props) => {
-  const URL = "http://localhost:8000";
-  const [studentNewName, setStudentNewName] = useState();
-  const [isClicked, setIsClicked] = useState(false);
+  const URL = "http://localhost:8000"; // API URL
+  const [studentNewName, setStudentNewName] = useState(); // State to store the new name of the student
+  const [isClicked, setIsClicked] = useState(false); // State to store if the pencil icon has been clicked or not
 
-  const { clickedStudent, setClickedStudent, loadStudents } = props;
+  // Destructuring props
+  const { clickedStudent, setClickedStudent, loadStudents } = props; 
 
-  const newNameRef = useRef();
+  // Ref to access the input element
+  const newNameRef = useRef(); 
 
+  // Function to handle the change of the student's name
   function ChangeName() {
-    let newName = newNameRef.current.value;
-    let nameRegex = /^([a-zA-ZÀ-ÿ]+)(\s[a-zA-ZÀ-ÿ]+)?\s([a-zA-ZÀ-ÿ]+)$/;
-    //console.log(newNameRef.current.value);
+    let newName = newNameRef.current.value; // Get the new name from the input
+    let nameRegex = /^([a-zA-ZÀ-ÿ]+)(\s[a-zA-ZÀ-ÿ]+)?\s([a-zA-ZÀ-ÿ]+)$/; // Regular expression for name validation
+    // If the new name does not match the name format
     if (!nameRegex.test(newName)) {
-      swal("Write your first optional middle and last name ONLY");
+      swal("Write your first name, optional middle, and last name with one space in between ONLY!"); // Show an error alert
     } else {
+      // Update the state with the new name and other details
       setStudentNewName({
         name: `${newName}`,
         cohort_name: sessionStorage.getItem("currentClass"),
         oldName: clickedStudent,
       });
-      setClickedStudent(newName);
+      setClickedStudent(newName); // Update the clicked student's name
+      // Send a PATCH request to the API to update the student's name
       fetch(`${URL}/api/students/nameChange`, {
         method: "PATCH",
         headers: {
@@ -38,21 +44,25 @@ export const ChangeStudentName = (props) => {
           oldName: clickedStudent,
         }),
       }).then(() => {
-        swal("Name Changed!");
-        loadStudents(sessionStorage.getItem("currentClass"));
+        swal("Name Changed!"); // Show a success alert
+        loadStudents(sessionStorage.getItem("currentClass")); // Load the updated list of students
       });
     }
   }
-  //console.log(studentNewName);
+
+  // Function to handle the click event on the pencil icon
   const handleClick = () => {
-    setIsClicked(!isClicked);
+    setIsClicked(!isClicked); // Flip the value of the isClicked state
   };
 
   return (
     <div className="pen-change-name">
+      {/* This component displays the pencil icon for changing student name */}
       <a className="pen" onClick={handleClick}>
         <BsPencilFill />
       </a>
+      {/* This component displays the input field for the new name and save button if the pencil is clicked, 
+      otherwise it displays the text "Change Name" */}
       {isClicked ? (
         <div className="pen-input">
           <input
--- app/src/tests/createCohort.js
+++ app/src/tests/createCohort.js
@@ -1,94 +1,119 @@
-
 const { By, Key, Builder } = require("selenium-webdriver");
 require("chromedriver");
-
+// Log4js is required for logging purposes
 var log4js = require("log4js");
 var logger = log4js.getLogger();
+// Setting the log level to debug
 logger.level = "debug";
 
-async function test_case_login() {
+// Defining the main function for testing the "create cohort" feature
+async function test_case_createCohort() {
+  // Creating a new browser instance using the Selenium WebDriver
   let driver = await new Builder().forBrowser("chrome").build();
 
+  // Navigating to the login page
   await driver.get("http://localhost:3001/login");
   logger.info("webpage has been oppened");
 
+  // Entering the username
   await driver
     .findElement(By.xpath("(//input[@class='form-control'])[1]"))
     .sendKeys("mansoor@yahoo.com");
   logger.info("username added");
 
+  // Entering the password and logging in
   await driver
     .findElement(By.xpath("(//input[@class='form-control'])[2]"))
     .sendKeys("password123", Key.RETURN);
   logger.info("Loged in the page");
 
+  // Setting the implicit wait time
   await driver.manage().setTimeouts({ implicit: 2000 });
+  // Clicking on the dropdown menu
+  await driver.findElement(By.id("dropdown-menu-align-end")).click();
+  logger.info("Clieked dropdown");
+
+  // Clicking on the "create cohort" button
+  await driver.findElement(By.id("btn-create-cohort")).click();
+  logger.info("Clicked Create Cohort");
+
+  // Entering the cohort name
   await driver
-  .findElement(By.id("dropdown-menu-align-end")).click();
- 
-//logger.info("username added");
-await driver.manage().setTimeouts({ implicit: 2000 });
-await driver
-.findElement(By.id("btn-create-cohort")).click();
+    .findElement(By.xpath("(//input[@class='form-control'])[1]"))
+    .sendKeys("MCSP-20");
+  logger.info("added Cohort Name");
 
-//logger.info("username added");
+// Entering the instructor name
+  await driver
+    .findElement(By.xpath("(//input[@class='form-control'])[2]"))
+    .sendKeys("David");
+  logger.info("added instructor");
 
-await driver
-.findElement(By.xpath("(//input[@class='form-control'])[1]"))
-.sendKeys("MCSP-18");
-//logger.info("username added");
+// Entering the start date
+  await driver
+    .findElement(By.xpath("(//input[@class=''])[1]"))
+    .sendKeys("02/20/2023", Key.RETURN);
+  logger.info("added start date");
 
-await driver
-.findElement(By.xpath("(//input[@class='form-control'])[2]"))
-.sendKeys("David");
-//logger.info("username added");
+// Entering the end date
+  await driver
+    .findElement(By.xpath("(//input[@class=''])[2]"))
+    .sendKeys("09/22/2023", Key.RETURN, Key.RETURN);
+  logger.info("added end date");
 
-await driver
-.findElement(By.xpath("(//input[@class=''])[1]"))
-.sendKeys("02/20/2023", Key.RETURN);
-//logger.info("Loged in the page");
-await driver.manage().setTimeouts({ implicit: 2000 });
-
-await driver
-.findElement(By.xpath("(//input[@class=''])[2]"))
-.sendKeys("09/22/2023", Key.RETURN, Key.RETURN);
-//logger.info("username added");
-
-await driver.manage().setTimeouts({ implicit: 2100 });
-await driver
-.findElement(By.xpath("(//button[@class='btn btn-primary'])[2]")).click();
+// Submitting the form
+  await driver
+    .findElement(By.xpath("(//button[@class='btn btn-primary'])[2]"))
+    .click();
+  logger.info("submit clickecd");
 
 
+  let result = driver.findElement(By.className("swal-text")).getText();
 
-  async function example() {
-    await driver.manage().setTimeouts({ implicit: 2000 });
+// Define an async function passFail that sets implicit timeouts and checks for the result
+  async function passFail() {
+    driver.manage().setTimeouts({ implicit: 2000 });
     let result = false;
+
+   
     try {
-      result = await driver.findElement(By.className("swal-text")).isDisplayed();
+       // Get the text of the element with class "swal-text"
+      result = await driver.findElement(By.className("swal-text")).getText();
+     // If the result equals "Cohort was succesfully created", log "test passed"
+      if (result === "Cohort was succesfully created") {
+        logger.info("test passed");
+        return true;
+      } else {
+        // If the result is not as expected, take a screenshot and write it to the imgs folder
+        driver.takeScreenshot().then(function (image) {
+          require("fs").writeFileSync("./imgs/addCohort.png", image, "base64");
+        });
+      }
     } catch {
+    // If an error occurs, log "test failed"
       console.log("test failed");
     }
-
-    if (result) {
-      logger.info("test passed");
-    } else {
-      driver.takeScreenshot().then(function (image) {
-        require("fs").writeFileSync("./imgs/addCohort.png", image, "base64");
-      });
-    }
-
-    await driver.manage().setTimeouts({ implicit: 2100 });
-await driver
-.findElement(By.className("swal-text")).click();
+    // Click the element with class "swal-text"
+    await driver.findElement(By.className("swal-text")).click();
   }
+// Call the passFail function
+passFail();
 
-  example();
+// Click the button with class "swal-button swal-button--confirm" found by xpath
+  await driver
+    .findElement(
+      By.xpath("(//button[@class='swal-button swal-button--confirm'])")
+    )
+    .click();
+  logger.info("ok clicked for successful add");
 
+  
+// Set a timeout of 5000 milliseconds before quitting the browser
   setTimeout(function () {
     logger.debug("quitting browser");
 
     driver.quit();
   }, 5000);
 }
-
-test_case_login();
+// Call the test case createCohort
+test_case_createCohort();
