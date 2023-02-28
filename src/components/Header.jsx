70c198bff63efc2e1379fe01b9024b07fecd2766
Matt Brooks <115656591+mattbrooks100@users.noreply.github.com>
2023-02-28 17:28:00 -0500

    updated navbar styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 import Logo from "../images/galvanize-logo.svg";
 import { AdminMenu } from "./AdminMenu";
+import { Container, Nav, Navbar, NavDropdown, Button } from "react-bootstrap";
 
 export const Header = (props) => {
   const { courses, isLoadingCourses, isLoggedIn, setIsLoggedIn } = props;
 
   return (
-    <header>
-      <div className="flex bg-gray-500">
-        <img src={Logo} alt="Galvanize Logo" height={50} className="ml-4"/>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container fluid>
+          <div id="navbar-left-container">
+            <Navbar.Brand href="/" className="navbar-title">
+              Galvanize Instructor Hub
+            </Navbar.Brand>
+            <Nav className="nav-links">
+              <Nav.Link href="/">Dashboard</Nav.Link>
+              <Nav.Link href="/projects">Projects</Nav.Link>
+              <Nav.Link href="/assessments">Assessments</Nav.Link>
+            </Nav>
+          </div>
+          <AdminMenu
+            courses={courses}
+            isLoggedIn={isLoggedIn}
+            setIsLoggedIn={setIsLoggedIn}
+            className="admin-menu"
+          />
+        </Container>
+      </Navbar>
+      {/* <header>
+        <img src={Logo} alt="Galvanize Logo" height={50} />
         <AdminMenu courses={courses} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-      </div>
-    </header>
+      </header> */}
+    </>
   );
 };
